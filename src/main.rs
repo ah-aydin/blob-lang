1355c50cab36e1576a588ddb83efe2a431bdafc3
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2024-01-07 14:44:13 +0200

    Update macro locations

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,40 @@ mod parser;
 use parser::{Parser, ParserStatus};
 use std::env;
 
-fn main() {
+fn cmain() -> i32 {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         println!("Usage: blob-lang [source_file]");
-        return;
+        return 1;
     }
+    let file_name = args.get(1).unwrap();
 
-    match Parser::new(args.get(1).unwrap()).unwrap().parse() {
-        ParserStatus::Succeeded(stmts) => stmts.iter().for_each(|stmt| println!("{:?}", stmt)),
-        ParserStatus::Failed => eprintln!("ERROR: Parsing failed"),
-        ParserStatus::IOError => eprintln!("ERROR: Failed while reading chunk from file"),
-    };
+    let parser = Parser::new(file_name);
+    if parser.is_err() {
+        eprintln!(
+            "Failed to read file {}\n{}",
+            file_name,
+            parser.err().unwrap()
+        );
+        return 1;
+    }
+    let mut parser = parser.unwrap();
+    match parser.parse() {
+        ParserStatus::Succeeded(stmts) => {
+            stmts.iter().for_each(|stmt| println!("{:?}", stmt));
+            0
+        }
+        ParserStatus::Failed => {
+            eprintln!("ERROR: Parsing failed");
+            1
+        }
+        ParserStatus::IOError => {
+            eprintln!("ERROR: Failed while reading chunk from file");
+            1
+        }
+    }
+}
+
+fn main() {
+    std::process::exit(cmain());
 }
