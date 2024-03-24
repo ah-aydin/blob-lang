999f28f140ec89d63788f6ce98ce2e64ee5ae5f9
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2024-03-24 14:19:28 +0200

    Notes

--- blobc/src/semantic_analysis/mod.rs
+++ blobc/src/semantic_analysis/mod.rs
@@ -1,6 +1,9 @@
-use crate::ast::{stmt::Stmt, AstWalker};
+use crate::ast::{
+    expr::{Expr, ExprBinaryOp, ExprBooleanOp, ExprCall, ExprUnaryOp},
+    stmt::{Stmt, StmtAssign, StmtFuncDecl, StmtIf, StmtIfElse, StmtVarDecl, StmtWhile},
+    AstWalker,
+};
 
-// TODO finish implementing this
 pub fn analyze(stmts: &Vec<Stmt>) -> Result<(), ()> {
     let mut analyzer = Analyzer::new(stmts);
     analyzer.analyze();
@@ -12,6 +15,7 @@ pub fn analyze(stmts: &Vec<Stmt>) -> Result<(), ()> {
 
 #[derive(Debug, Default)]
 enum AnalyzerError {
+    #[default]
     Undefined,
 }
 
@@ -29,87 +33,72 @@ impl<'a> Analyzer<'a> {
     }
 
     pub fn analyze(&mut self) {
-        self.walk(&self.stmts);
+        // let _ = self.walk(&self.stmts);
     }
 }
 
 impl<'a> AstWalker<(), AnalyzerError> for Analyzer<'a> {
-    fn func(&mut self, func_decl: &crate::ast::stmt::StmtFuncDecl) -> Result<(), AnalyzerError> {
-        todo!()
+    fn func(&mut self, func_decl: &StmtFuncDecl) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for func")
     }
 
     fn block_stmt(&mut self, stmts: &Vec<Stmt>) -> Result<(), AnalyzerError> {
-        todo!()
+        todo!("Implement semantic analysis for block")
     }
 
-    fn expr_stmt(&mut self, expr: &crate::ast::expr::Expr) -> Result<(), AnalyzerError> {
-        todo!()
+    fn expr_stmt(&mut self, expr: &Expr) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for expr stmt")
     }
 
-    fn return_stmt(&mut self, expr: &Option<crate::ast::expr::Expr>) -> Result<(), AnalyzerError> {
-        todo!()
+    fn return_stmt(&mut self, expr: &Option<Expr>) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for return stmt")
     }
 
-    fn if_stmt(&mut self, iff: &crate::ast::stmt::StmtIf) -> Result<(), AnalyzerError> {
-        todo!()
+    fn if_stmt(&mut self, iff: &StmtIf) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for if stmt")
     }
 
-    fn if_else_stmt(
-        &mut self,
-        if_else: &crate::ast::stmt::StmtIfElse,
-    ) -> Result<(), AnalyzerError> {
-        todo!()
+    fn if_else_stmt(&mut self, if_else: &StmtIfElse) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for if else stmt")
     }
 
-    fn var_decl_stmt(
-        &mut self,
-        var_decl: &crate::ast::stmt::StmtVarDecl,
-    ) -> Result<(), AnalyzerError> {
-        todo!()
+    fn var_decl_stmt(&mut self, var_decl: &StmtVarDecl) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for var decl stmt")
     }
 
-    fn assign_stmt(&mut self, assign: &crate::ast::stmt::StmtAssign) -> Result<(), AnalyzerError> {
-        todo!()
+    fn assign_stmt(&mut self, assign: &StmtAssign) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for assign stmt")
     }
 
-    fn while_stmt(&mut self, whilee: &crate::ast::stmt::StmtWhile) -> Result<(), AnalyzerError> {
-        todo!()
+    fn while_stmt(&mut self, whilee: &StmtWhile) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for while stmt")
     }
 
     fn bool_expr(&mut self, b: bool) -> Result<(), AnalyzerError> {
-        todo!()
+        todo!("Implement semantic analysis for bool expr")
     }
 
     fn i32_expr(&mut self, number: &str) -> Result<(), AnalyzerError> {
-        todo!()
+        todo!("Implement semantic analysis for i32 expr")
     }
 
     fn identifier_expr(&mut self, name: &str) -> Result<(), AnalyzerError> {
-        todo!()
+        todo!("Implement semantic analysis for identifier expr")
     }
 
-    fn unary_expr(
-        &mut self,
-        unary_op: &crate::ast::expr::ExprUnaryOp,
-    ) -> Result<(), AnalyzerError> {
-        todo!()
+    fn unary_expr(&mut self, unary_op: &ExprUnaryOp) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for unary expr")
     }
 
-    fn binary_expr(
-        &mut self,
-        binary_op: &crate::ast::expr::ExprBinaryOp,
-    ) -> Result<(), AnalyzerError> {
-        todo!()
+    fn binary_expr(&mut self, binary_op: &ExprBinaryOp) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for binary expr")
     }
 
-    fn boolean_expr(
-        &mut self,
-        boolean_op: &crate::ast::expr::ExprBooleanOp,
-    ) -> Result<(), AnalyzerError> {
-        todo!()
+    fn boolean_expr(&mut self, boolean_op: &ExprBooleanOp) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for boolean expr")
     }
 
-    fn call(&mut self, call: &crate::ast::expr::ExprCall) -> Result<(), AnalyzerError> {
-        todo!()
+    fn call(&mut self, call: &ExprCall) -> Result<(), AnalyzerError> {
+        todo!("Implement semantic analysis for call expr")
     }
 }
