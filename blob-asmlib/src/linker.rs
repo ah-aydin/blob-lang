d018df93a732e5890d1e3f9a6057459909d03245
ah-aydin <ahamzaaydin@gmail.com>
2024-07-14 16:00:53 +0300

    Store labels on linkind TODO implement these fully after adding in memory

--- blob-asmlib/src/linker.rs
+++ blob-asmlib/src/linker.rs
@@ -1,4 +1,6 @@
-use blob_bc::{Ins, InsArg};
+use std::{collections::HashMap, slice::Iter};
+
+use blob_bc::{Ins, InsArg, OpCode};
 use blob_common::error;
 
 use crate::token::{Token, TokenType};
@@ -30,86 +32,90 @@ use crate::token::{Token, TokenType};
 ///
 /// hlt -> "HTL"
 ///
+/// memory -> "[" (reg | label) "]"
 /// imd -> "#" NUMBER
 /// reg -> "R"[0-31]
 /// label -> IDENTIFIER
 /// ```
 struct Linker {
     tokens: Vec<Token>,
+    label_to_instruction: HashMap<String, usize>,
 }
 
 impl Linker {
     fn new(tokens: Vec<Token>) -> Linker {
-        Linker { tokens }
+        Linker {
+            tokens,
+            label_to_instruction: HashMap::new(),
+        }
     }
 
     fn link(&mut self) -> Result<Vec<u8>, ()> {
         let mut t_iter = self.tokens.iter();
-        let mut instructions: Vec<Ins> = Vec::new();
+        let mut instructions: Vec<u8> = Vec::new();
 
         while let Some(token) = t_iter.next() {
-            let op_code = match token.token_type {
-                TokenType::Op(op_code) => op_code,
+            match &token.token_type {
+                TokenType::Op(op_code) => instructions
+                    .append(&mut self.link_op(&token, *op_code, &mut t_iter)?.to_bytes()),
+                TokenType::LabelDecl(lbl) => {
+                    self.label_to_instruction
+                        .insert(lbl.clone(), instructions.len());
+                }
                 TokenType::EOF => break,
                 _ => todo!("{:?}", token),
             };
-
-            let args_types = op_code.get_args_types();
-            let args_count = args_types.len();
-            let mut args: Vec<InsArg> = Vec::with_capacity(3);
-
-            for (i, arg_types) in args_types.iter().enumerate() {
-                let arg = t_iter.next();
-                if arg.is_none() {
-                    error!(
-                        "{} Expected {args_count} argument for instruction {:?} but {i} where given",
-                        token.file_coords,
-                        op_code
-                    );
-                    return Err(());
-                }
-
-                let token = arg.unwrap();
-                let token_arg_type = token.token_type.get_ins_arg_type();
-                if token_arg_type.is_none() {
-                    error!(
-                        "{} Got unexpected token '{:?}' as argument for OpCode '{:?}'",
-                        token.file_coords, token, op_code
-                    );
-                    return Err(());
-                }
-                let token_arg_type = token_arg_type.unwrap();
-
-                if !arg_types.contains(&token_arg_type) {
-                    error!(
-                        "{} Argument {} for OpCode '{:?}' cannot be '{:?}'",
-                        token.file_coords,
-                        i + 1,
-                        op_code,
-                        token_arg_type
-                    );
-                    return Err(());
-                }
-                let arg = match &token.token_type {
-                    TokenType::Reg(reg) => InsArg::Reg(*reg),
-                    TokenType::ImdVal(imd) => InsArg::Imd(*imd as u16),
-                    TokenType::LabelUsg(_) => todo!(),
-                    _ => unreachable!("How did we get here?"),
-                };
-                args.push(arg);
-            }
-
-            let ins = Ins::from_ins_args(op_code, args);
-            instructions.push(ins);
         }
 
-        Ok(instructions
-            .iter()
-            .map(|ins| ins.to_bytes())
-            .collect::<Vec<Vec<u8>>>()
-            .into_iter()
-            .flatten()
-            .collect())
+        Ok(instructions)
+    }
+
+    fn link_op(&self, token: &Token, op_code: OpCode, t_iter: &mut Iter<Token>) -> Result<Ins, ()> {
+        let args_types = op_code.get_args_types();
+        let args_count = args_types.len();
+        let mut args: Vec<InsArg> = Vec::with_capacity(3);
+
+        for (i, arg_types) in args_types.iter().enumerate() {
+            let arg = t_iter.next();
+            if arg.is_none() {
+                error!(
+                    "{} Expected {args_count} argument for instruction {:?} but {i} where given",
+                    token.file_coords, op_code
+                );
+                return Err(());
+            }
+
+            let token = arg.unwrap();
+            let token_arg_type = token.token_type.get_ins_arg_type();
+            if token_arg_type.is_none() {
+                error!(
+                    "{} Got unexpected token '{:?}' as argument for OpCode '{:?}'",
+                    token.file_coords, token.token_type, op_code
+                );
+                return Err(());
+            }
+            let token_arg_type = token_arg_type.unwrap();
+
+            if !arg_types.contains(&token_arg_type) {
+                error!(
+                    "{} Argument {} for OpCode '{:?}' cannot be '{:?}'",
+                    token.file_coords,
+                    i + 1,
+                    op_code,
+                    token_arg_type
+                );
+                return Err(());
+            }
+            let arg = match &token.token_type {
+                TokenType::Reg(reg) => InsArg::Reg(*reg),
+                TokenType::ImdVal(imd) => InsArg::Imd(*imd as u16),
+                TokenType::LabelUsg(_) => todo!(),
+                _ => unreachable!("How did we get here?"),
+            };
+            args.push(arg);
+        }
+
+        Ok(Ins::from_ins_args(op_code, args))
     }
 }
 
